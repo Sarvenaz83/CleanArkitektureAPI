ed2ebc32dc7bdf9fb8b31b8fccdd6a38ca764432
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2023-11-29 14:06:33 +0100

    In this branch I create all command and query tests for cats.

---
+++ Test/CatTests/QueryTest/GetAllCatsQueryHandlerTest.cs
@@ -0,0 +1,33 @@
+﻿using Application.Queries.Cats.GetAll;
+using Domain.Models;
+using Infrastructure.Database;
+
+namespace Test.CatTests.QueryTest
+{
+    public class GetAllCatsQueryHandlerTest
+    {
+        private MockDatabase _mockDatabase;
+        private GetAllCatsQueryHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new GetAllCatsQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_GetAllCats_FromDatabase_ReturnsCorrect()
+        {
+            //Arrange
+            var getAllCatQuery = new GetAllCatsQuery();
+
+            //Act
+            var result = await _handler.Handle(getAllCatQuery, CancellationToken.None);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<Cat>>(result);
+        }
+    }
+}
